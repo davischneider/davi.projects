455c016a2738a25c1e213442ef9d874341cd7246
davischneider <83461000+davischneider@users.noreply.github.com>
2021-05-04 10:22:08 -0300

    Add files via upload

---
+++ Exercícios Power Point/10/exe10.js
@@ -0,0 +1,3 @@
+numero = 9;
+resultado = Math.sqrt (numero)
+alert(`A raíz quadrada de ${numero} é ${resultado}`);
---
+++ Exercícios Power Point/11/exe11.js
@@ -0,0 +1,6 @@
+var catAd = prompt("Digite o valor do cateto adjacente:");
+var catOp = prompt("Digite o valor do cateto oposto:");
+
+hipotenusa = Math.sqrt((catAd*catAd)+(catOp*catOp));
+
+alert(`O valor da hipotenusa é ${hipotenusa}`);
